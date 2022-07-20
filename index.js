9039cbf989a489949d8b1d5242bc4017bce6e90b
nguyentranchan <nguyentranchan@gogojungle.vn>
2022-07-20 18:32:50 +0700

    test

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const Peer = require("simple-peer")
-const pear = new Peer({
+const wrtc = require("wrtc")
+;
+const peer = new Peer({
     initiator: location.hash === '#init',
-    trickle: false
-}
-)
+    wrtc: wrtc,
+    trickle: false,
+    objectMode: true
+})
 
-pear.on('signal', function (data) {
+peer.on('signal', function (data) {
+  console.log('signal new peer', data)
   document.getElementById('yourId').value = JSON.stringify(data)
 })
 
 document.getElementById('connect').addEventListener('click', function () {
   const otherId = JSON.parse(document.getElementById('otherId').value)
-  pear.signal(otherId)
+  peer.signal(otherId)
 })
 
 
 document.getElementById('send').addEventListener('click', function () {
   const yourMessage = document.getElementById('yourMessage').value
-  pear.send(yourMessage)
+  peer.send(yourMessage)
 })
 
-pear.on('data', function (data) {
+peer.on('data', function (data) {
+  console.log('data')
   document.getElementById('message').textContent += data + '\n'
 })
