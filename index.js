deaee3c8cdf2df40607f45c07a744ac7be4bbf25
nguyentranchan <nguyentranchan@gogojungle.vn>
2022-07-21 11:32:02 +0700

    test

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const Peer = require("simple-peer")
-const pear = new Peer({
+// var getUserMedia = require('getusermedia')
+//
+// getUserMedia({ video: true, audio: false }, function (err, stream) {
+//   if (err) return console.error(err)
+
+  const Peer = require('simple-peer')
+  const peer = new Peer({
     initiator: location.hash === '#init',
-    trickle: false
-}
-)
+    trickle: false,
+    // stream: stream
+  })
 
-pear.on('signal', function (data) {
-  document.getElementById('yourId').value = JSON.stringify(data)
-})
+  peer.on('signal', function (data) {
+    console.log("signal")
+    document.getElementById('yourId').value = JSON.stringify(data)
+  })
 
-document.getElementById('connect').addEventListener('click', function () {
-  const otherId = JSON.parse(document.getElementById('otherId').value)
-  pear.signal(otherId)
-})
+  document.getElementById('connect').addEventListener('click', function () {
+    console.log("connect button clicked")
+    var otherId = JSON.parse(document.getElementById('otherId').value)
+    peer.signal(otherId)
+  })
 
+  document.getElementById('send').addEventListener('click', function () {
+    console.log("send button")
+    var yourMessage = document.getElementById('yourMessage').value
+    peer.send(yourMessage)
+  })
 
-document.getElementById('send').addEventListener('click', function () {
-  const yourMessage = document.getElementById('yourMessage').value
-  pear.send(yourMessage)
-})
-
-pear.on('data', function (data) {
-  document.getElementById('message').textContent += data + '\n'
-})
+  peer.on('data', function (data) {
+    console.log("data")
+    document.getElementById('messages').textContent += data + '\n'
+  })
+//
+//   peer.on('stream', function (stream) {
+//     console.log("stream")
+//     var video = document.createElement('video')
+//     document.body.appendChild(video)
+//
+//     video.srcObject = stream
+//     video.play()
+//   })
+// })
